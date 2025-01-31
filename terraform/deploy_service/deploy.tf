f72b2ec7a64a5858ebfa27a9133a92b5d9176dd2
NewEnergyCode <matveychuk.m@gmail.com>
2025-01-31 14:31:43 +0200

    add .env

--- terraform/deploy_service/deploy.tf
+++ terraform/deploy_service/deploy.tf
@@ -156,6 +156,6 @@ resource "aws_lb_target_group" "http_ecs_meeting_tg" {
     unhealthy_threshold = 2
     interval            = 60
     protocol            = "HTTP"
-    path                = "/health"
+    path                = "/api/v1/docs"
   }
 }
